e55d75ca942e46e4f97a5164fa7db0574187c491
hiro-account <niidaatgm01@gmail.com>
2024-06-23 08:03:59 +0900

    24/06/23朝

--- cmn/MyDB.php
+++ cmn/MyDB.php
@@ -4,35 +4,35 @@ class MyDB {
     private $mysqli;
     private $mode;
     private $count;
-    
+
     // コンストラクタ
     function __construct() {
         // DB接続
         $this->mysqli = new mysqli('localhost', 'root', '', 'y240608_01');
-        
+
         if ($this->mysqli->connect_error) {
         } else {
             $this->mysqli->set_charset('utf8');
         }
     }
-    
+
     // デストラクタ
     function __destruct() {
         $this->mysqli->close();
     }
-    
+
     function query($prm_query) {
-        $result = $this->mysqli->multi_query($prm_query);
-        
+        $result = $this->mysqli->query($prm_query);
+
         if ($result === FALSE) {
             $error = $this->mysqli->errno . ':' . $this->mysqli->error;
-            
+
             return array ('status' => FALSE,
                 'count' => 0,
-                'resutl' => '',
+                'result' => '',
                 'error' => $error);
         }
-        
+
         if ($result === TRUE) {
             // SELECT文以外
             return array ('status' => TRUE,
@@ -41,23 +41,31 @@ class MyDB {
                 'error' => '');
         } else {
             // SELECT文
-            
-            
-            
-            
-            
+            $num_rows = $result->num_rows;
+            $data = array();
+
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+
+            $result->close();
+
+            return array('status' => TRUE,
+                'count' => $num_rows,
+                'result' => $data,
+                'error' => '');
         }
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
+
     }
-    
-    
+
+
 }
 
 
--- staff/staff_top_ctrl.php
+++ staff/staff_top_ctrl.php
@@ -2,6 +2,7 @@
 $to_cmn = dirname(__FILE__) . '/../cmn/';
 // require_once($to_cmn . 'const.php');
 require_once($to_cmn . 'func.php');
+require_once $to_cmn . 'MyDB.php';
 
 //TODO:定数移動
 const READ_FAILED = '<p>スタッフ一覧読み出し失敗（システム障害発生）</p>';
@@ -21,38 +22,54 @@ function get_content() {
     $last_staff_id = NULL;
 
     try {
-        $mysqli = new mysqli('localhost', 'root', '', 'y240608_01');
+//         $mysqli = new mysqli('localhost', 'root', '', 'y240608_01');
 
-        if ($mysqli->connect_error) {
-            return READ_FAILED;
-        } else {
-            $mysqli->set_charset('utf8');
-        }
+//         if ($mysqli->connect_error) {
+//             return READ_FAILED;
+//         } else {
+//             $mysqli->set_charset('utf8');
+//         }
 
         $query =<<< EOQ
-SELECT m.id AS id , m.last_name AS last_name, m.first_name AS first_name
-FROM m_staff_for_dev AS m INNER JOIN t_logical_delete_for_dev AS d ON m.id = d.id
-WHERE d.flag = FALSE
-ORDER BY m.id
-EOQ;
+        SELECT m.id AS id , CONCAT(m.last_name, m.first_name) AS name
+        FROM m_staff_for_dev AS m INNER JOIN t_logical_delete_for_dev AS d ON m.id = d.id
+        WHERE d.flag = FALSE
+        ORDER BY m.id
+        EOQ;
 
-        $result = $mysqli->query($query);
+//         $result = $mysqli->query($query);
 
-        while (TRUE) {
-            $rslt_arr = $result->fetch_assoc();
+//         while (TRUE) {
+//             $rslt_arr = $result->fetch_assoc();
 
-            if (!$rslt_arr) {
-                break;
+//             if (!$rslt_arr) {
+//                 break;
+//             }
+
+//             $last_staff_id = $rslt_arr['id'];
+//             $staff_list .= '<tr><td>' . $last_staff_id . '</td><td>' . $rslt_arr['last_name'] . $rslt_arr['first_name'] . '</td>'
+//                 . '<td class="t-a-c"><input type="submit" name="staff_id_' . $last_staff_id . '" value="表示"></td></tr>' . LF;
+
+//                 if (!isset($first_staff_id)) {
+//                     $first_staff_id = $last_staff_id;
+//                 }
+//         }
+        $db = new MyDB();
+        $tmp = $db->query($query);
+
+        foreach ($tmp['result'] as $value) {
+            $last_staff_id = $value['id'];
+//             $staff_list .= '<tr><td>' . $last_staff_id . '</td><td>' . $value['last_name'] . $value['first_name'] . '</td>' . '<td class="t-a-c"><input type="submit" name="staff_id_' . $last_staff_id . '" value="表示"></td></tr>' . LF;
+            $staff_list .= '<tr><td>' . $last_staff_id . '</td><td>' . $value['name'] . '</td><td class="t-a-c"><input type="submit" name="staff_id_' . $last_staff_id . '" value="表示"></td></tr>' . LF;
+
+            if (! isset($first_staff_id)) {
+                $first_staff_id = $last_staff_id;
             }
-
-            $last_staff_id = $rslt_arr['id'];
-            $staff_list .= '<tr><td>' . $last_staff_id . '</td><td>' . $rslt_arr['last_name'] . $rslt_arr['first_name'] . '</td>'
-                . '<td class="t-a-c"><input type="submit" name="staff_id_' . $last_staff_id . '" value="表示"></td></tr>' . LF;
-
-                if (!isset($first_staff_id)) {
-                    $first_staff_id = $last_staff_id;
-                }
         }
+
+
+
+
     } catch (Exception $e) {
         return READ_FAILED . LF;
     } finally {
@@ -60,7 +77,7 @@ EOQ;
         if (isset($result)) {
             $result->close();
         }
-        
+
         if (isset($mysqli)) {
             $mysqli->close();
         }
