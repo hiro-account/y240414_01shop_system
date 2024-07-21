2e9db678c41cb20fcaac9fda1dfdb29acfbfb4cd
hiro-at-github <niida0306@gmail.com>
2024-07-22 08:35:57 +0900

    24/07/22/08時30分台

--- staff/staff_create_confirm_ctrl.php
+++ staff/staff_create_confirm_ctrl.php
@@ -24,7 +24,17 @@ function get_content($prm_post)
         , 'rdo_privilege' => L_PRIVILEGE                     // 10
     );
 
-    $from = isset($prm_post[N_ID]) ? FROM_UPDATE : FROM_CREATE;
+    //240722次回スタッフ更新にfromを持たせて判別から
+    // 遷移元(スタッフ更新orスタッフ登録の何れか)
+    // $from = isset($prm_post[N_ID]) ? FROM_UPDATE : FROM_CREATE;
+    $is_from_create = FALSE;
+    $is_from_update = FALSE;
+
+    if (isset($prm_post[N_ID])) {
+        $is_from_create = TRUE;
+    } else {
+        $is_from_update = TRUE;
+    }
 
     $item_arr = array(
         N_LAST_NAME => new Item(// 氏
@@ -33,63 +43,63 @@ function get_content($prm_post)
             $prm_post[N_LAST_NAME],
             STR_EMPTY,
             NOT_ENTERED,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_LAST_NAME] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_LAST_NAME] : NULL
         ), N_FIRST_NAME => new Item(// 名
             N_FIRST_NAME,
             L_FIRST_NAME,
             $prm_post[N_FIRST_NAME],
             STR_EMPTY,
             NOT_ENTERED,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_FIRST_NAME] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_FIRST_NAME] : NULL
         ), N_LAST_NAME_KANA => new Item(// 氏(カナ)
             N_LAST_NAME_KANA,
             L_LAST_NAME . L_KANA,
             $prm_post[N_LAST_NAME_KANA],
             STR_EMPTY,
             NOT_ENTERED,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_LAST_NAME_KANA] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_LAST_NAME_KANA] : NULL
         ), N_FIRST_NAME_KANA => new Item(// 名(カナ)
             N_FIRST_NAME_KANA,
             L_FIRST_NAME . L_KANA,
             $prm_post[N_FIRST_NAME_KANA],
             STR_EMPTY,
             NOT_ENTERED,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_FIRST_NAME_KANA] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_FIRST_NAME_KANA] : NULL
         ), N_SEX => new Item(// 性別
             N_SEX,
             L_SEX,
             $prm_post[N_SEX],
             NULL,
             NULL,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_SEX] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_SEX] : NULL
         ), N_BIRTH_YEAR => new Item(// 生年月日の年
             N_BIRTH_YEAR,
             L_BIRTH_DATE . L_YEAR,
             $prm_post[N_BIRTH_YEAR],
             '0000',
             UNSELECTED,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_BIRTH_YEAR] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_BIRTH_YEAR] : NULL
         ), N_BIRTH_MONTH => new Item(// 生年月日の月
             N_BIRTH_MONTH,
             L_BIRTH_DATE . L_MONTH,
             $prm_post[N_BIRTH_MONTH],
             '00',
             UNSELECTED,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_BIRTH_MONTH] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_BIRTH_MONTH] : NULL
         ), N_BIRTH_DAY => new Item(// 生年月日の日
             N_BIRTH_DAY,
             L_BIRTH_DATE . L_DAY,
             $prm_post[N_BIRTH_DAY],
             '00',
             UNSELECTED,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_BIRTH_DAY] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_BIRTH_DAY] : NULL
         ), N_PRIVILEGE => new Item(// 権限
             N_PRIVILEGE,
             L_PRIVILEGE,
             isset($prm_post[N_PRIVILEGE]) ? $prm_post[N_PRIVILEGE] : NULL,
             NULL,
             UNSELECTED,
-            strcmp($from, FROM_UPDATE) === I_0 ? $prm_post[N_PREV . N_PRIVILEGE] : NULL
+            $is_from_update ? $prm_post[N_PREV . N_PRIVILEGE] : NULL
         )
     );
 
@@ -97,13 +107,16 @@ function get_content($prm_post)
     $changed_item_arr = array();
 
     foreach ($item_arr as $value) {
+        // 値のサニタイズと前後スペースの除去
         $value->convert_sp_char_and_trim();
+        // 未入力、未選択の項目の確認
         $err_msg = $value->check_unenter_unslct_value();
 
         if (isset($err_msg)) {
             $empty_msgs .= add_p($err_msg) . LF;
         }
 
+        // 未変更の項目の確認(スタッフ更新の場合)
         $is_value_changed = $value->check_value_changed();
 
         if (isset($is_value_changed)) {
@@ -111,13 +124,13 @@ function get_content($prm_post)
         }
     }
 
-    var_dump($changed_item_arr);
+    // var_dump($changed_item_arr);
 
 
     $sex_arr = array('-', '男', '女');
     $privilege_arr = array('O' => '一般', 'A' => '管理者');
 
-    $create_or_update = strcmp($from, FROM_UPDATE) === I_0 ? '更新' : '登録';
+    $create_or_update = $is_from_update ? '更新' : '登録';
     $html_h2 = "<h2>スタッフ{$create_or_update}</h2>";
 
 
@@ -127,7 +140,7 @@ function get_content($prm_post)
     }
 
     //TODO:パスワードのチェックを削除し生年月日のチェックだけとなったため、エラーメッセージ表示の処理の更新を検討
-    $invalid_msg = NULL;
+    $invalid_birthday_msg = NULL;
 
     // 生年月日の妥当性のチェック
     if (!checkdate(
@@ -135,59 +148,21 @@ function get_content($prm_post)
         intval($item_arr[N_BIRTH_DAY]->get_verified_value()),
         intval($item_arr[N_BIRTH_YEAR]->get_verified_value())
     )) {
-        $invalid_msg .= add_p(L_BIRTH_DATE . 'が不正') . LF;
+        $invalid_birthday_msg .= add_p(L_BIRTH_DATE . 'が不正') . LF;
     }
 
-    if (isset($invalid_msg)) {
+    if (isset($invalid_birthday_msg)) {
         // 生年月日に不備がある場合、エラーメッセージを表示する
-        return $html_h2 . LF . $invalid_msg;
+        return $html_h2 . LF . $invalid_birthday_msg;
     }
 
     //TODO:下記に限らず変数名整理
     $html_id = NULL;
     $form_action = NULL;
 
-    // if (count($separated_arr['hidden']) > 0) {
-    //     // スタッフ更新からの遷移である場合
-    //     //TODO:下記処理の概要記載
-    //     $removed_arr = array();
-
-    //     foreach ($separated_arr['input'] as $key => $value) {
-    //         $pos = strpos($key, '_');
-
-    //         if (!$pos) {
-    //             $removed_arr[$key] = $value;
-    //         } else {
-    //             $removed_arr[substr($key, $pos + I_1)] = $value;
-    //         }
-    //     }
-
-    //     $cnt = I_0;
-
-    //     foreach ($separated_arr['hidden'] as $key => $value) {
-    //         $pos = strpos($key, '_');
-    //         $removed_value = $removed_arr[substr($key, $pos + I_1)];
-
-    //         if (strcmp($removed_value, $value) === I_0) {
-    //             $cnt++;
-    //         }
-    //     }
-
-    //     if (count($separated_arr['hidden']) === $cnt) {
-    //         return $html_h2 . LF . add_p('項目が一つも変更されていない') . LF;
-    //     }
-
-    //     $html_id = LF . '<tr><td>スタッフID</td><td>：' . $separated_arr['input']['id'] . '</td></tr>';
-    //     $form_action = './staff_update_done.php';
-    // } else {
-    //     // スタッフ登録からの遷移である場合
-    //     //TODO:下記処理の概要記載
-    //     $html_id = '';
-    //     $form_action = './staff_create_done.php';
-    // }
 
 
-    if (strcmp($from, FROM_UPDATE) === I_0) {
+    if ($is_from_update) {
          // スタッフ更新からの遷移である場合
         //TODO:下記処理の概要記載
         if (count($changed_item_arr) === I_0) {
@@ -202,7 +177,7 @@ function get_content($prm_post)
 
         $html_id = LF . '<tr><td>スタッフID</td><td>：' . $separated_arr['input']['id'] . '</td></tr>';
         $form_action = './staff_update_done.php';
-    } else if (strcmp($from, FROM_CREATE) === I_0) {
+    } else if ($is_from_create) {
         // スタッフ登録からの遷移である場合
         //TODO:下記処理の概要記載
         $html_id = '';
